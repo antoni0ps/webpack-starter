1b35f5523ef0b2cf775a877617098203f344b3d8
Antonio <anpisan@gmail.com>
2022-04-30 09:49:03 +0200

    docs creado

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,15 +1,17 @@
+import { DEFAULT_STATS } from 'webpack-dev-server';
 import '../css/componentes.css'
 // import webpackLogo from '../assets/img/webpack-logo.png'
 
 //Debemos agregar la palabra reservada export a todo lo que queramos exportar a otro archivo js.
 export const saludar = (nombre) => {
+
     console.log('Creando etiqueta h1');
 
     const h1 = document.createElement('h1');
     h1.innerText = `Hola, ${nombre}`;
 
     document.body.append(h1);
-
+zxcvzxcvxczv
 
     const imagen = document.createElement('img');
     imagen.src = webpackLogo;
